7a868327637202ad811280ceda0297339cc5a45b
jinicha <jinichaa@gmail.com>
2021-11-15 22:03:15 -0800

    Create a graph definition

--- main.py
+++ main.py
@@ -5,11 +5,25 @@ import config
 token = config.token
 username = config.username
 base_url = "https://pixe.la/v1/users"
-params = {
+create_user_params = {
     "token": token,
     "username": username,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
-response = requests.post(url=base_url, json=params)
+
+# create a graph definition
+create_graph_url = f'{base_url}/{username}/graphs'
+request_header = {
+    "X-USER-TOKEN": token
+}
+request_body = {
+    "id": "agraph",
+    "name": "drink water",
+    "unit": "oz",
+    "type": "int",
+    "color": "sora"
+}
+
+response = requests.post(url=create_graph_url, json=request_body, headers=request_header)
 print(response.text)
